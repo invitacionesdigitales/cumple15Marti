8e288b8513b34b9750def4f965b86deb1aee9670
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2024-08-08 19:48:55 -0300

    cuatro

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.9581099682496!2d-58.50496522487762!3d-34.73145026425415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccef8ac7acf83%3A0x308e92157265a081!2sGolf%20Club%20Eventos!5e0!3m2!1ses-419!2sar!4v1719444128246!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.333218643956!2d-58.4468157!3d-34.84785370000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd411755c173d%3A0x96ce6a0b9aa6e832!2sDo%C3%B1a%20Olga%20Eventos!5e0!3m2!1ses-419!2sar!4v1723157147809!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
