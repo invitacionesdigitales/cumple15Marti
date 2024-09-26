bb596fded5ea189fff1596fc6bfcf2f9dd8bf97c
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2024-09-26 18:05:11 -0300

    seis

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-let diaEvento = new Date('September 14, 2024 21:30:00').getTime();
+let diaEvento = new Date('November 09, 2024 21:30:00').getTime();
 let boton = document.querySelector("#btn")
 
 
